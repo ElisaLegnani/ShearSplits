c34e7d4e84ba32cbe7f89d5bae7e1e089309a097
Elisa Legnani <elegnani97@gmail.com>
2025-06-30 07:57:29 -0700

    Fix directories

--- cosmosis_runs/submit_cosmosis_demo.sh
+++ cosmosis_runs/submit_cosmosis_demo.sh
@@ -5,7 +5,7 @@ export RUNNAME=test
 export BASE=/global/cfs/cdirs/des/elisa/cosmosis-standard-library
 source /dvs_ro/cfs/projectdirs/des/zuntz/cosmosis-global/setup-cosmosis3
 
-export DATAFILE=/global/u1/e/elegnani/ShearSplits/cosmosis_runs/output_dir/DESshear.fits
+export DATAFILE=/global/u1/e/elegnani/ShearSplits/cosmosis_runs/input_dir/DESshear.fits
 export VALUESINCLUDE=/global/u1/e/elegnani/ShearSplits/cosmosis_runs/cosmosis_config/values_cosmo.ini
 export OUT_DIR=/global/u1/e/elegnani/ShearSplits/cosmosis_runs/output_dir/
 
